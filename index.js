02846a7368533d114e75c0bb3e7f92075e031dce
Josep <jrosich@gmail.com>
2023-11-08 09:33:41 +0100

    Folder creation
    The script now checks if there is a folder with the current date to store the screenshots. If it doesn't exist, it creates it.

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
-// load Puppeteer
-const { exit } = require("process");
-const pt = require("puppeteer");
+const fs = require('fs');
+const pt = require('puppeteer');
 
-// get current date/time
-let date_time = new Date();
+// Function to create a directory if it doesn't exist
+const createDirectoryIfNotExists = (dir) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+};
 
-// get current date
-// adjust 0 before single digit date
-let day = ("0" + date_time.getDate()).slice(-2);
-
-// get current month
-let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
-
-// get current year
-let year = date_time.getFullYear();
-
-// get current hours
-let hours = date_time.getHours();
-
-// get current minutes
-let minutes = date_time.getMinutes();
-
-// get current seconds
-let seconds = date_time.getSeconds();
-
-// prints date in YYYY-MM-DD format
-const sc_date = (year + "-" + month + "-" + day + "-" + hours + "-" + minutes + "-" + seconds);
- 
-// this script accepts a url as an argument
-let url = new URL(process.argv[2]);
-// the filename will be extracted from the hostname of the url
-let filename = `./snapshots/${url.hostname}-${sc_date}.png`;
- 
+// Function to delay execution
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-let scw = Number(process.argv[3]);
-let sch = Number(process.argv[4]);
+// Get current date
+const currentDate = new Date();
+const year = currentDate.getFullYear().toString();
+const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+const day = currentDate.getDate().toString().padStart(2, '0');
 
+// Construct folder name with current date
+const folderName = `${year}-${month}-${day}`;
+const screenshotsDir = `./snapshots/${folderName}`;
 
-if (scw < 1900 || scw > 3000) scw = 1900;
-if (sch < 1000 || sch > 6000) sch = 1000;
+// Create the directory if it doesn't exist
+createDirectoryIfNotExists(screenshotsDir);
 
+// Get other date/time components for the file name
+const hours = currentDate.getHours().toString().padStart(2, '0');
+const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+const seconds = currentDate.getSeconds().toString().padStart(2, '0');
 
-// launch Puppeteer and add image parameters
-pt.launch().then(async (browser) => {
-  const p = await browser.newPage();
- 
-  // capture an image that is 1000 x 500 pixels
-  await p.setViewport({ width: scw, height: sch });
- 
-  // navigate to the site you would like to capture
-  await p.goto(url, { waitUntil: "domcontentloaded" });
- 
-  delay(5000).then(async () => {
-    // take the screenshot of the site and save it to the current folder
-    await p.screenshot({ path: filename });
- 
-    // close the Puppeteer browser when you are finished.
-    await browser.close();
- 
-    console.log("done");
-  });
-});
+// Construct file name with the date and time
+const sc_date = `${year}-${month}-${day}-${hours}-${minutes}-${seconds}`;
+
+// Accept URL as an argument
+let url = new URL(process.argv[2]);
+
+// The filename will be extracted from the hostname of the url
+let filename = `${screenshotsDir}/${url.hostname}-${sc_date}.png`;
+
+// Read screenshot dimensions from arguments, with default values
+let scw = parseInt(process.argv[3], 10) || 1900;
+let sch = parseInt(process.argv[4], 10) || 1000;
+
+// Validate dimensions
+scw = scw >= 1900 && scw <= 3000 ? scw : 1900;
+sch = sch >= 1000 && sch <= 6000 ? sch : 1000;
+
+// Launch Puppeteer and add image parameters
+(async () => {
+  const browser = await pt.launch();
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: scw, height: sch });
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await delay(5000); // wait for 5 seconds
+
+  await page.screenshot({ path: filename });
+  await browser.close();
+
+  console.log(`Screenshot saved: ${filename}`);
+})();
